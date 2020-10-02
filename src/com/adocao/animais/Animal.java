32d36f68384e3f22949cae56e08569d43cd8c178
Willeeer <willersabino@gmail.com>
2020-10-01 22:46:44 -0300

    criacao de atributos na classe Animal

--- src/com/adocao/animais/Animal.java
+++ src/com/adocao/animais/Animal.java
@@ -3,4 +3,26 @@ package com.adocao.animais;
 public class Animal
 {
 
+   private String nome;
+   private String sexo;
+
+   public String getNome()
+   {
+      return nome;
+   }
+
+   public void setNome(String nome)
+   {
+      this.nome = nome;
+   }
+
+   public String getSexo()
+   {
+      return sexo;
+   }
+
+   public void setSexo(String sexo)
+   {
+      this.sexo = sexo;
+   }
 }
